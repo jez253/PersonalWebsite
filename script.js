fc1d6d991148139c9502cda15337e9b05e07bd8a
Jose Zurita <jose95@vt.edu>
2019-07-17 11:41:54 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+$(window).scroll(function() {
+
+    var $window = $(window),
+        $body = $('body'),
+        $panel = $('.part');
+  
+   
+    var scroll = $window.scrollTop() + ($panel.height() / 3);
+   
+    $panel.each(function () {
+      var $this = $(this);
+  
+      if ($this.position().top <= scroll && $this.position().top + $this.height() > scroll) {
+   
+        var bgColor = $(this).data('background');
+        $body.css('background-color', bgColor);
+      }
+    });
+  
+  }).scroll();
+
+  var logoHeight = $('#parallax img').height();
+    if (logoHeight < 104) {
+        var margintop = (104 - logoHeight) / 2;
+        $('#parallax img').css('margin-top', margintop);
+    }
