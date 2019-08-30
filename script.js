9b97f1770c51eb017f1a8b6ab3e0e03c76ee1e3c
Jose Zurita <jose95@vt.edu>
2019-08-29 22:29:59 -0400

    div errors fixed

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-$(window).scroll(function() {
+// $(window).scroll(function() {
 
-    var $window = $(window),
-        $body = $('body'),
-        $panel = $('.part');
+//     var $window = $(window),
+//         $body = $('body'),
+//         $panel = $('.part');
   
    
-    var scroll = $window.scrollTop() + ($panel.height() / 3);
+//     var scroll = $window.scrollTop() + ($panel.height() / 3);
    
-    $panel.each(function () {
-      var $this = $(this);
+//     $panel.each(function () {
+//       var $this = $(this);
   
-      if ($this.position().top <= scroll && $this.position().top + $this.height() > scroll) {
+//       if ($this.position().top <= scroll && $this.position().top + $this.height() > scroll) {
    
-        var bgColor = $(this).data('background');
-        $body.css('background-color', bgColor);
-      }
-    });
+//         var bgColor = $(this).data('background');
+//         $body.css('background-color', bgColor);
+//       }
+//     });
   
-  }).scroll();
+//   }).scroll();
 
-  var logoHeight = $('#parallax img').height();
-    if (logoHeight < 104) {
-        var margintop = (104 - logoHeight) / 2;
-        $('#parallax img').css('margin-top', margintop);
-    }
+//   var logoHeight = $('#parallax img').height();
+//     if (logoHeight < 104) {
+//         var margintop = (104 - logoHeight) / 2;
+//         $('#parallax img').css('margin-top', margintop);
+//     }
